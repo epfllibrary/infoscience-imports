03c125fd44524af44061126e217015c97d44fbc0
Julien Sicot <julien.sicot@epfl.ch>
2024-09-13 10:05:15 +0200

    disabled some doctype not desired

--- mappings.py
+++ mappings.py
@@ -11,10 +11,10 @@ doctypes_mapping_dict = {
         "Article": "Journal articles",
         "Book Chapter": "Books and Book parts",
         "Book": "Books and Book parts",
-        "Correction": "Journal articles",
+        # "Correction": "Journal articles",
         "Editorial Material":"Journal articles",
         "Letter": "Journal articles",
-        "Meeting Abstract": "Conferences, Workshops, Symposiums, and Seminars",
+        # "Meeting Abstract": "Conferences, Workshops, Symposiums, and Seminars",
         "Proceedings Paper": "Conferences, Workshops, Symposiums, and Seminars",
         "Review": "Journal articles"
     },
@@ -61,27 +61,29 @@ doctypes_mapping_dict = {
     }
 }
 
-collections_mapping = {'Patents': 'ce5a1b89-cfb3-40eb-bdd2-dcb021e755b7',
- 'Projects': '49ec7e96-4645-4bc0-a015-ba4b81669bbc',
- 'Teaching Materials': 'c7e018d4-2349-46dd-a8a4-c32cf5f5f9a1',
- 'Images, Videos, Interactive resources, and Design': '329f8cd3-dc1a-4228-9557-b27366d71d41',
- 'Newspaper, Magazine, or Blog post': '971cc7fa-b177-46e3-86a9-cfac93042e9d',
- 'Funding': '8b185e36-0f99-4669-9a46-26a19d4f3eab',
- 'Other': '0066acb2-d5c0-49a0-b273-581df34961cc',
- 'Datasets and Code': '33a1cd32-7980-495b-a2bb-f34c478869d8',
- 'Student works': '305e3dad-f918-48f6-9309-edbeb7cced14',
- 'Units': 'bc85ee71-84b0-4f78-96a1-bab2c50b7ac9',
- 'Contents': 'e8dea11e-a080-461b-82ee-6d9ab48404f3',
- 'Virtual collections': '78f331d1-ee55-48ef-bddf-508488493c90',
- 'EPFL thesis': '4af344ef-0fb2-4593-a234-78d57f3df621',
- 'Reports, Documentation, and Standards': 'd5ec2987-2ee5-4754-971b-aca7ab4f9ab7',
- 'Preprints and Working Papers': 'd8dada3a-c4bd-4c6f-a6d7-13f1b4564fa4',
- 'Books and Book parts': '1a71fba2-2fc5-4c02-9447-f292e25ce6c1',
- 'Persons': '6acf237a-90d7-43e2-82cf-c3591e50c719',
- 'Events': '6e2af01f-8b92-461e-9d08-5e1961b9a97b',
- 'Conferences, Workshops, Symposiums, and Seminars': 'e91ecd9f-56a2-4b2f-b7cc-f03e03d2643d',
- 'Journals': '9ada82da-bb91-4414-a480-fae1a5c02d1c',
- 'Journal articles': '8a8d3310-6535-4d3a-90b6-2a4428097b5b'}
+collections_mapping = {
+    "Patents": "ce5a1b89-cfb3-40eb-bdd2-dcb021e755b7",
+    "Projects": "49ec7e96-4645-4bc0-a015-ba4b81669bbc",
+    "Teaching Materials": "c7e018d4-2349-46dd-a8a4-c32cf5f5f9a1",
+    "Images, Videos, Interactive resources, and Design": "329f8cd3-dc1a-4228-9557-b27366d71d41",
+    "Newspaper, Magazine, or Blog post": "971cc7fa-b177-46e3-86a9-cfac93042e9d",
+    "Funding": "8b185e36-0f99-4669-9a46-26a19d4f3eab",
+    "Other": "0066acb2-d5c0-49a0-b273-581df34961cc",
+    "Datasets and Code": "33a1cd32-7980-495b-a2bb-f34c478869d8",
+    "Student works": "305e3dad-f918-48f6-9309-edbeb7cced14",
+    "Units": "bc85ee71-84b0-4f78-96a1-bab2c50b7ac9",
+    "Contents": "e8dea11e-a080-461b-82ee-6d9ab48404f3",
+    "Virtual collections": "78f331d1-ee55-48ef-bddf-508488493c90",
+    "EPFL thesis": "4af344ef-0fb2-4593-a234-78d57f3df621",
+    "Reports, Documentation, and Standards": "d5ec2987-2ee5-4754-971b-aca7ab4f9ab7",
+    "Preprints and Working Papers": "d8dada3a-c4bd-4c6f-a6d7-13f1b4564fa4",
+    "Books and Book parts": "1a71fba2-2fc5-4c02-9447-f292e25ce6c1",
+    "Persons": "6acf237a-90d7-43e2-82cf-c3591e50c719",
+    "Events": "6e2af01f-8b92-461e-9d08-5e1961b9a97b",
+    "Conferences, Workshops, Symposiums, and Seminars": "e91ecd9f-56a2-4b2f-b7cc-f03e03d2643d",
+    "Journals": "9ada82da-bb91-4414-a480-fae1a5c02d1c",
+    "Journal articles": "8a8d3310-6535-4d3a-90b6-2a4428097b5b",
+}
 
 def normalize_doctype_value(source, value):
     # Testing the function
