34659d0567617d4fb9b0b1d51bdbfa1b2a745b7e
Alain Borel <alain.borel@epfl.ch>
2024-11-14 14:57:13 +0100

    config for EPFL community on Zenodo

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ default_queries = {
     "wos": "OG=(Ecole Polytechnique Federale de Lausanne)",
     "scopus": "AF-ID(60028186) OR AF-ID(60210159) OR AF-ID(60070536) OR AF-ID(60204330) OR AF-ID(60070531) OR AF-ID(60070534) OR AF-ID(60070538) OR AF-ID(60014951) OR AF-ID(60070529) OR AF-ID(60070532) OR AF-ID(60070535) OR AF-ID(60122563) OR AF-ID(60210160) OR AF-ID(60204331)",
     "openalex": "OPENALEX_QUERY_HERE",  # Placeholder for OpenAlex query
-    "zenodo": 'parent.communities.entries.id:"3c1383da-d7ab-4167-8f12-4d8aa0cc637f"']
+    "zenodo": 'parent.communities.entries.id:"3c1383da-d7ab-4167-8f12-4d8aa0cc637f"',
 }
 
 # Define the order of the sources during the deduplication process
@@ -17,12 +17,25 @@ source_order = ["scopus", "wos", "zenodo"]
 unit_types = ["Laboratoire", "Groupe", "Chaire"]
 
 # Scopus : EPFL labs internal IDs
-scopus_epfl_afids = ["60028186","60210159","60070536","60204330","60070531",
-                     "60070534","60070538","60014951","60070529","60070532",
-                     "60070535","60122563","60210160","60204331"]
+scopus_epfl_afids = [
+    "60028186",
+    "60210159",
+    "60070536",
+    "60204330",
+    "60070531",
+    "60070534",
+    "60070538",
+    "60014951",
+    "60070529",
+    "60070532",
+    "60070535",
+    "60122563",
+    "60210160",
+    "60204331",
+]
 
 # License conditions for Unpaywall
 LICENSE_CONDITIONS = {
     "allowed_licenses": ["cc-by", "public-domain"],
-    "allowed_oa_statuses": ["gold", "hybrid", "green"]  # Added allowed OA statuses
+    "allowed_oa_statuses": ["gold", "hybrid", "green"],  # Added allowed OA statuses
 }
