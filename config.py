1bb72f2e3686fc07f92079321a74ba3550f4dae6
Julien Sicot <julien.sicot@epfl.ch>
2026-02-23 11:54:25 +0100

    add epo query in config

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ default_queries = {
     "openalex": "authorships.institutions.lineage:i5124864|i4210122261|i4210129591,is_retracted:false,is_paratext:false",
     "zenodo": 'parent.communities.entries.id:"3c1383da-d7ab-4167-8f12-4d8aa0cc637f"',
     "crossref": '{"query.affiliation": "EPFL"}',
-    "epo": '(pa all "ECOLE POLYTECHNIQUE FED LAUSANNE EPFL" AND pd >20231231) NOT cl any "US"',
+    "epo": 'pa all "ECOLE POLYTECH* FED LAUSANNE*" AND (pn=EP OR pn=WO)',
 }
 
 # Define the order of the sources during the deduplication process
@@ -26,6 +26,7 @@ source_order = [
     "openalex",
     "datacite",
     "zenodo",
+    "epo",
 ]
 
 # Define types of unit to retrieve in priority from api.epfl.ch
